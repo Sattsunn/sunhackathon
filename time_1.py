01f6ad05c89f20e1bf30585686317c91e54fd7fd
Sattsunn <satsusarii@gmail.com>
2024-01-12 13:51:10 +0900

    deployしたい

--- time_1.py
+++ time_1.py
@@ -1,8 +1,5 @@
-from slacker import Slacker
+import crowmwhether
 from crontab import CronTab
-
-slack = Slacker("SLACK_BOT_TOKEN")
-
  
 class CrontabControl:
     def __init__(self):
@@ -26,12 +23,11 @@ class CrontabControl:
         self.read_jobs(file)
         for result in self.all_job.run_scheduler():
             # スケジュールになるとこの中の処理が実行される
-            slack.chat.post_message('timeline', 'crontab使えてるよ', as_user=True)
             print("予定していたスケジュールを実行しました")
  
 def main():
-    command = 'python3 time_1.py'
-    schedule = '45 13 * * *'
+    command = 'python3 ./cronweather.py'
+    schedule = '55 13 * * *'
     file = 'output.tab'
  
     # インスタンス作成
@@ -40,9 +36,55 @@ def main():
     c.write_job(command, schedule, file)
     # タスクスケジュールの監視を開始
     c.monitor_start(file)
-
+ 
 if __name__ == "__main__":
     main()
+
+# from slacker import Slacker
+# from crontab import CronTab
+
+# slack = Slacker("SLACK_BOT_TOKEN")
+
+ 
+# class CrontabControl:
+#     def __init__(self):
+#         self.cron = CronTab()
+#         self.job = None
+#         self.all_job = None
+ 
+#     # ファイルにジョブを書き込むメソッド
+#     def write_job(self, command, schedule, file_name):
+#         self.job = self.cron.new(command=command)
+#         self.job.setall(schedule)
+#         self.cron.write(file_name)
+ 
+#     # ファイル中のジョブを全て読み込むメソッド
+#     def read_jobs(self, file_name):
+#         self.all_job = CronTab(tabfile=file_name)
+ 
+#     # ジョブを監視するメソッド
+#     def monitor_start(self, file):
+#         # スケジュールを読み込む
+#         self.read_jobs(file)
+#         for result in self.all_job.run_scheduler():
+#             # スケジュールになるとこの中の処理が実行される
+#             slack.chat.post_message('timeline', 'crontab使えてるよ', as_user=True)
+#             print("予定していたスケジュールを実行しました")
+ 
+# def main():
+#     command = 'python3 time_1.py'
+#     schedule = '45 13 * * *'
+#     file = 'output.tab'
+ 
+#     # インスタンス作成
+#     c = CrontabControl()
+#     # ファイルに書き込む
+#     c.write_job(command, schedule, file)
+#     # タスクスケジュールの監視を開始
+#     c.monitor_start(file)
+
+# if __name__ == "__main__":
+#     main()
 
 # #WEB_HOOK_URLに、さっき発行したURLを設定
 # WEB_HOOK_URL = "https://hooks.slack.com/services/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"
